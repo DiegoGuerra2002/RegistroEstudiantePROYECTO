193e8bb12dc004b4be8daaa8d4903b4d20f15286
Agustin <Agustin@DiegoPC>
2022-04-08 22:30:25 -0600

    Se han añadido comentarios y se ha agregado un arreglo en el que se guardan variables polimorficas, ademas de que se crea un codigo para ingresar informacion que sera ingresada al arreglo.

--- src/registro/de/estudiantes/Becamain.java
+++ src/registro/de/estudiantes/Becamain.java
@@ -1,6 +1,10 @@
 package registro.de.estudiantes;
 
+//La clase beca se a instanciado en la clase Becamain
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.*;
 public class Becamain {
     
         public static void main(String[] args) {
@@ -8,6 +12,22 @@ public class Becamain {
         Beca estudiante = new Beca("Walter","Vanegas",3,20,26,10);
         
         estudiante.mostrarDATOS();
+        //Se crea un arreglo en el que se guarden variables polimorficas ( Lista )
+        List<Notas> lista = new ArrayList<>();
+        
+        Scanner reader = new Scanner(System.in);
+        System.out.println("La nota del estudiante: ");
+        double nota = reader.nextDouble();
+        //Intanciar=Llamar un metodo de otra clase a la clase main.
+        lista.add(new Vnota(nota));
+        
+        System.out.println("Registro de notas");
+        for(Notas salida:lista) {
+            salida.notaver();
+        }
+        
+        System.out.println("------------");
+        ((Vnota)lista).Casting();
     }
     
 }
